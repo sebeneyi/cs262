ccbdfd3184c9634887107367df6a020f9ec1bb2a
Sebene <yimail08@gmail.com>
2020-09-25 23:14:58 -0400

    homework1, added features

--- homework1/my-project/components/header.js
+++ homework1/my-project/components/header.js
@@ -3,8 +3,22 @@ import { View, Text, StyleSheet } from 'react-native';
 
 export default function Header() {
   return (
-    <View>
-      <Text>My Todos</Text>
+    <View style={styles.header}>
+      <Text style={styles.title} >My Todos</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  header: {
+    height: 80,
+    paddingTop: 38,
+    backgroundColor: 'coral'
+  },
+  title: {
+    textAlign: 'center',
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
--- homework1/my-project/components/todoItem.js
+++ homework1/my-project/components/todoItem.js
@@ -1,13 +1,29 @@
 import React from 'react'
-import {StyleSheet, TouchableOpacity, Text, View} from 'react-native';
+import { StyleSheet, TouchableOpacity, Text, View } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 
-export default function TodoItem({ pressHandler, item }) {
+export default function TodoItem({ item, pressHandler }) {
   return (
     <TouchableOpacity onPress={() => pressHandler(item.key)}>
-      <View>
-        <Text>{item.text}</Text>
+      <View style={styles.item}>
+        <MaterialIcons name='delete' size={18} color='#333' />
+        <Text style={styles.itemText}>{item.text}</Text>
       </View>
     </TouchableOpacity>
   )
 };
+
+const styles = StyleSheet.create({
+  item: {
+    padding: 16,
+    marginTop: 16,
+    borderColor: '#bbb',
+    borderWidth: 1,
+    borderStyle: 'dashed',
+    borderRadius: 10,
+    flexDirection: 'row',
+  },
+  itemText: {
+    marginLeft: 10,
+  }
+});
