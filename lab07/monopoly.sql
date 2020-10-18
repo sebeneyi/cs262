9bccffecdffa1741c0bb68af64c81d2a7e4ce1e4
Sebene <yimail08@gmail.com>
2020-10-18 01:35:59 -0400

    lab07

--- lab07/monopoly.sql
+++ lab07/monopoly.sql
@@ -5,33 +5,63 @@
 -- @version Summer, 2015
 --
 
--- Drop previous versions of the tables if they they exist, in reverse order of foreign keys.
+-- Drop previous versions of the tables if they exist, in reverse order of foreign keys.
 DROP TABLE IF EXISTS PlayerGame;
 DROP TABLE IF EXISTS Game;
 DROP TABLE IF EXISTS Player;
 
 -- Create the schema.
 CREATE TABLE Game (
-	ID integer PRIMARY KEY, 
+	ID integer PRIMARY KEY,
 	time timestamp
 	);
 
 CREATE TABLE Player (
-	ID integer PRIMARY KEY, 
+	ID integer PRIMARY KEY,
 	emailAddress varchar(50) NOT NULL,
 	name varchar(50)
 	);
 
 CREATE TABLE PlayerGame (
-	gameID integer REFERENCES Game(ID), 
+	gameID integer REFERENCES Game(ID),
 	playerID integer REFERENCES Player(ID),
 	score integer
 	);
+
+CREATE TABLE PlayerCash (
+	playerID integer REFERENCES Player(ID),
+	cash integer
+);
+
+CREATE TABLE Properties (
+	playerID integer REFERENCES Player(ID),
+	ID integer PRIMARY KEY
+);
+
+CREATE TABLE Houses (
+	propertyID integer REFERENCES Properties(ID),
+	houses integer
+);
+
+CREATE TABLE Hotels (
+	propertyID integer REFERENCES Properties(ID),
+	hotels integer
+);
+
+CREATE TABLE CurrentLocation (
+	playerID integer REFERENCES Player(ID),
+	currentLocation integer
+);
 
 -- Allow users to select data from the tables.
 GRANT SELECT ON Game TO PUBLIC;
 GRANT SELECT ON Player TO PUBLIC;
 GRANT SELECT ON PlayerGame TO PUBLIC;
+GRANT SELECT ON PlayerCash TO PUBLIC;
+GRANT SELECT ON Properties TO PUBLIC;
+GRANT SELECT ON Houses TO PUBLIC;
+GRANT SELECT ON Hotels TO PUBLIC;
+GRANT SELECT ON CurrentLocation TO PUBLIC;
 
 -- Add sample records.
 INSERT INTO Game VALUES (1, '2006-06-27 08:00:00');
@@ -50,3 +80,26 @@ INSERT INTO PlayerGame VALUES (2, 2, 0.00);
 INSERT INTO PlayerGame VALUES (2, 3, 500.00);
 INSERT INTO PlayerGame VALUES (3, 2, 0.00);
 INSERT INTO PlayerGame VALUES (3, 3, 5500.00);
+
+INSERT INTO PlayerCash VALUES (1, 200);
+INSERT INTO PlayerCash VALUES (2, 10);
+INSERT INTO PlayerCash VALUES (3, 89);
+
+INSERT INTO Properties VALUES (1, 1);
+INSERT INTO Properties VALUES (1, 2);
+INSERT INTO Properties VALUES (2, 3);
+INSERT INTO Properties VALUES (2, 4);
+INSERT INTO Properties VALUES (3, 5);
+INSERT INTO Properties VALUES (3, 6);
+
+INSERT INTO Houses VALUES (1, 0);
+INSERT INTO Houses VALUES (3, 8);
+INSERT INTO Houses VALUES (5, 2);
+
+INSERT INTO Hotels VALUES (2, 3);
+INSERT INTO Hotels VALUES (4, 0);
+INSERT INTO Hotels VALUES (6, 3);
+
+INSERT INTO CurrentLocation VALUES (1, 0);
+INSERT INTO CurrentLocation VALUES (2, 5);
+INSERT INTO CurrentLocation VALUES (3, 8);
